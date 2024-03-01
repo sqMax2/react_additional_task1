7f737fecdc7b8e4b3ff1b7e520d605ff9d86d029
Sergei <msvp@mail.ru>
2024-03-01 18:44:17 +0300

    chore: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { useViewportSize } from './useViewportSize';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Demo/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
